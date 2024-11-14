215ff49f1c7fa98b0d7a42e6ad8453fe653fb214
Shiming Zhang <wzshiming@hotmail.com>
2024-11-14 10:52:29 +0800

    Add get banned ip

---
+++ get-banned-ip/go.mod
@@ -0,0 +1,42 @@
+module github.com/wzshiming/public-image-mirror-tools/get-banned-ip
+
+go 1.23.3
+
+require (
+	github.com/spf13/pflag v1.0.5
+	github.com/wzshiming/accesslog v0.0.0-20240919142156-8846d033f7ef
+	github.com/wzshiming/accesslog-aliyun-oss v0.0.0-20240918101505-dff91d636532
+	github.com/wzshiming/csv-sqlite v0.0.0-20241113131724-5bf18b36f0cf
+	github.com/wzshiming/geario v0.0.0-20240308093553-a996e3817533
+	golang.org/x/sync v0.9.0
+)
+
+require (
+	github.com/aliyun/aliyun-oss-go-sdk v3.0.2+incompatible // indirect
+	github.com/antlr4-go/antlr/v4 v4.13.0 // indirect
+	github.com/dustin/go-humanize v1.0.1 // indirect
+	github.com/google/cel-go v0.21.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/hashicorp/golang-lru/v2 v2.0.7 // indirect
+	github.com/kr/pretty v0.3.1 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-sqlite3 v1.14.24 // indirect
+	github.com/ncruces/go-strftime v0.1.9 // indirect
+	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
+	github.com/stoewer/go-strcase v1.2.0 // indirect
+	github.com/wzshiming/easycel v0.6.0 // indirect
+	github.com/wzshiming/httpcache v0.4.2 // indirect
+	golang.org/x/exp v0.0.0-20231108232855-2478ac86f678 // indirect
+	golang.org/x/sys v0.22.0 // indirect
+	golang.org/x/time v0.6.0 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20230803162519-f966b187b2e5 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20230803162519-f966b187b2e5 // indirect
+	google.golang.org/protobuf v1.33.0 // indirect
+	modernc.org/gc/v3 v3.0.0-20240107210532-573471604cb6 // indirect
+	modernc.org/libc v1.55.3 // indirect
+	modernc.org/mathutil v1.6.0 // indirect
+	modernc.org/memory v1.8.0 // indirect
+	modernc.org/sqlite v1.33.1 // indirect
+	modernc.org/strutil v1.2.0 // indirect
+	modernc.org/token v1.1.0 // indirect
+)
---
+++ get-banned-ip/main.go
@@ -0,0 +1,268 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/spf13/pflag"
+	"github.com/wzshiming/accesslog"
+	accesslog_aliyun_oss "github.com/wzshiming/accesslog-aliyun-oss"
+	"github.com/wzshiming/accesslog/tocsv"
+	csv_sqlite "github.com/wzshiming/csv-sqlite"
+	"github.com/wzshiming/geario"
+	"golang.org/x/sync/errgroup"
+)
+
+const getSQL = `
+SELECT
+    RemoteIP AS ip,
+    count(*) AS request_count,
+    sum(BodySentBytes) AS got_bytes,
+    RequestPath AS path
+FROM csv
+GROUP BY
+    ip, path
+HAVING
+    (
+        request_count > 2 AND
+    	got_bytes > 2 * 1024 * 1024 * 1024
+    ) OR (
+        request_count > 8 AND
+    	got_bytes > 1024 * 1024 * 1024
+    ) OR (
+        request_count > 32 AND
+    	got_bytes > 100 * 1024 * 1024
+    )
+ORDER BY
+    ip DESC
+LIMIT 1000
+`
+
+var (
+	cache           = "./cache"
+	endpoint        string
+	bucket          string
+	accessKeyID     string
+	accessKeySecret string
+
+	startTime string
+	endTime   string
+
+	fields = []string{
+		"RemoteIP",
+		"RequestPath",
+		"BodySentBytes",
+	}
+	condition = `self.RemoteIP != "10.10.0.10" && self.Operation == "GetObject"`
+)
+
+var DataFormat = "2006-01-02-15"
+
+func init() {
+	now := time.Now()
+	endTime = now.Add(-time.Hour).Format(DataFormat)
+	startTime = now.AddDate(0, 0, -1).Format(DataFormat)
+
+	pflag.StringVar(&cache, "cache", cache, "cache")
+	pflag.StringVar(&endpoint, "endpoint", "", "endpoint")
+	pflag.StringVar(&bucket, "bucket", "", "bucket")
+	pflag.StringVar(&accessKeyID, "access-key-id", "", "access key id")
+	pflag.StringVar(&accessKeySecret, "access-key-secret", "", "access key secret")
+	pflag.Parse()
+}
+
+type info struct {
+	RequestCount int
+	GotBytes     geario.B
+	List         []string
+}
+
+type recorder struct {
+	list map[string]*info
+}
+
+func newRecorder() *recorder {
+	return &recorder{
+		list: map[string]*info{},
+	}
+}
+
+func (r *recorder) String() string {
+	keys := make([]string, 0, len(r.list))
+	for k := range r.list {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+	out := bytes.NewBuffer(nil)
+
+	w := csv.NewWriter(out)
+	for _, k := range keys {
+		v := r.list[k]
+		w.Write([]string{
+			k,
+			fmt.Sprintf("Duplicate Request Count %d, Bytes %s, all targets %v", v.RequestCount, v.GotBytes, cleanList(v.List)),
+		})
+	}
+	w.Flush()
+	return out.String()
+}
+
+func cleanList(s []string) []string {
+	sort.Strings(s)
+	for i := range s {
+		list := strings.Split(s[i], "/")
+		if len(list) > 2 {
+			l := list[len(list)-2]
+			if len(l) > 8 {
+				l = l[:8] + "..."
+			}
+			s[i] = l
+		}
+	}
+	return s
+}
+
+func (r *recorder) Write(record []string) error {
+	if record[0] == "ip" {
+		return nil
+	}
+
+	i := r.list[record[0]]
+	if i == nil {
+		i = &info{}
+	}
+
+	requestCount, err := strconv.Atoi(record[1])
+	if err != nil {
+		return err
+	}
+
+	i.RequestCount += requestCount
+
+	gotBytes, err := strconv.Atoi(record[2])
+	if err != nil {
+		return err
+	}
+
+	i.GotBytes += geario.B(gotBytes)
+
+	i.List = append(i.List, record[3])
+
+	r.list[record[0]] = i
+	return nil
+}
+
+func main() {
+	ctx := context.Background()
+	r, w := io.Pipe()
+
+	g, ctx := errgroup.WithContext(ctx)
+
+	g.Go(func() error {
+		defer w.Close()
+		return run(
+			fields,
+			condition,
+			startTime,
+			endTime,
+			endpoint,
+			bucket,
+			accessKeyID,
+			accessKeySecret,
+			cache,
+			w,
+		)
+	})
+
+	tmp := fmt.Sprintf("%s.%d.db", endTime, time.Now().Unix())
+
+	g.Go(func() error {
+		return csv_sqlite.CSV2DB(ctx, r, tmp, "csv")
+	})
+	err := g.Wait()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer os.Remove(tmp)
+
+	record := newRecorder()
+
+	err = csv_sqlite.FromDB(context.Background(), tmp, record, getSQL)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(record.String())
+
+}
+
+const dataFormat = "2006-01-02-15"
+
+func run(
+	fields []string,
+	condition string,
+	startTime string,
+	endTime string,
+	endpoint string,
+	bucketName string,
+	accessKeyID string,
+	accessKeySecret string,
+	cache string,
+	output io.Writer,
+) error {
+	end, err := time.Parse(dataFormat, endTime)
+	if err != nil {
+		return err
+	}
+
+	start, err := time.Parse(dataFormat, startTime)
+	if err != nil {
+		return err
+	}
+
+	cacheHTTPClient := accesslog_aliyun_oss.NewCacheHTTPClient(nil, cache)
+
+	client, err := accesslog_aliyun_oss.NewOSSClient(
+		endpoint,
+		accessKeyID,
+		accessKeySecret,
+		accesslog_aliyun_oss.HTTPClient(cacheHTTPClient),
+	)
+	if err != nil {
+		return err
+	}
+
+	ch := make(chan accesslog_aliyun_oss.AccessLogFormatted, 128)
+
+	go func() {
+		defer close(ch)
+		for i := start; i.Before(end); i = i.Add(time.Hour) {
+			date := i.Format(dataFormat) + "-"
+			err := accesslog_aliyun_oss.ProcessAccessLogWithClient(client, bucketName, date, func(entry accesslog.Entry[accesslog_aliyun_oss.AccessLog], err error) error {
+				if err != nil {
+					return err
+				}
+				f, err := entry.Entry().Formatted()
+				if err != nil {
+					return err
+				}
+				ch <- f
+				return nil
+			})
+			if err != nil {
+				log.Fatal("Error", err)
+			}
+		}
+	}()
+
+	return tocsv.ProcessToCSV[accesslog_aliyun_oss.AccessLogFormatted](output, condition, fields, ch)
+}
